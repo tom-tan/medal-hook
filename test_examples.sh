9ae47aa537b9aa626abf4ee21284d263255b2f00
Tomoya Tanjo <ttanjo@gmail.com>
2021-02-25 20:02:35 +0000

    Fix test_examples.sh

--- test_examples.sh
+++ test_examples.sh
@@ -2,16 +2,21 @@
 
 dub build -b release || exit 1
 
+code=0
+
 for ex in examples/*
 do
+    rm -f $ex/network-hooked.yml $ex/actual.txt
     ./bin/medal-hook $ex/network.yml --hook $ex/hook.yml > $ex/network-hooked.yml
     if [ $? -ne 0 ]; then
         echo "failed: $ex"
+        code=1
         continue
     fi
     medal $ex/network-hooked.yml -i $ex/init.yml --workdir=$ex --quiet > $ex/actual.txt
     if [ $? -ne 0 ]; then
         echo "failed: $ex"
+        code=1
         continue
     fi
     expected=$(cat $ex/expected.txt | md5sum -)
@@ -21,5 +26,8 @@ do
         rm -f $ex/network-hooked.yml $ex/actual.txt
     else
         echo "failed: $ex"
+        code=1
     fi
 done
+
+exit $code
