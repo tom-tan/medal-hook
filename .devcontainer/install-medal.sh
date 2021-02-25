dba482a8f11417e8f7446c32ce7e4638af8abe2d
Tomoya Tanjo <ttanjo@gmail.com>
2021-02-25 19:34:12 +0000

    Add install-medal.sh

---
+++ .devcontainer/install-medal.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+if [ $# -eq 1 ]; then
+    download_str="download/$1"
+else
+    download_str=latest/download
+fi
+
+curl -SL https://github.com/tom-tan/medal/releases/${download_str}/medal-linux-x86_64.tar.gz \
+    | tar xC /usr/bin
+
+curl -SL https://github.com/tom-tan/medal/releases/${download_str}/net2dot-linux-x86_64.tar.gz \
+    | tar xC /usr/bin
