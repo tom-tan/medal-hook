9bd3c0425f19bfc52bf445e64218430ce338d272
Tomoya Tanjo <ttanjo@gmail.com>
2021-02-25 21:01:59 +0000

    Add examples

--- test_examples.sh
+++ test_examples.sh
@@ -6,16 +6,28 @@ code=0
 
 for ex in examples/*
 do
-    rm -f $ex/network-hooked.yml $ex/actual.txt
+    rm -f $ex/network-hooked.yml $ex/subnetwork-hooked.yml $ex/actual.txt
     ./bin/medal-hook $ex/network.yml --hook $ex/hook.yml > $ex/network-hooked.yml
     if [ $? -ne 0 ]; then
-        echo "failed: $ex"
+        echo "hook failed: $ex"
         code=1
         continue
     fi
+
+    if [ -e $ex/subnetwork.yml ]; then
+        ./bin/medal-hook $ex/subnetwork.yml --hook $ex/hook.yml > $ex/subnetwork-hooked.yml
+        if [ $? -ne 0 ]; then
+            echo "sub hook failed: $ex"
+            code=1
+            continue
+        fi
+        sed -e 's/subnetwork.yml/subnetwork-hooked.yml/' $ex/network-hooked.yml > $ex/network-hooked1.yml
+        mv $ex/network-hooked1.yml $ex/network-hooked.yml
+    fi
+
     medal $ex/network-hooked.yml -i $ex/init.yml --workdir=$ex --quiet > $ex/actual.txt
     if [ $? -ne 0 ]; then
-        echo "failed: $ex"
+        echo "execution failed: $ex"
         code=1
         continue
     fi
@@ -23,9 +35,9 @@ do
     actual=$(cat $ex/actual.txt | md5sum -)
     if [ "$expected" = "$actual" ]; then
         echo "success: $ex"
-        rm -f $ex/network-hooked.yml $ex/actual.txt
+        rm -f $ex/network-hooked.yml $ex/subnetwork-hooked.yml $ex/actual.txt
     else
-        echo "failed: $ex"
+        echo "result failed: $ex"
         code=1
     fi
 done
